c926871aa6c75f87dfbb57ed69f832c5cd65d7a0
vondreele <vondreele@f090fa4f-6a81-4b03-ec04-e05dd2f8225b>
2009-08-28 19:37:10 +0000

    format fixes
    git-svn-id: https://subversion.xray.aps.anl.gov/pyFprime@40 f090fa4f-6a81-4b03-ec04-e05dd2f8225b

--- trunk/fprime.py
+++ trunk/fprime.py
@@ -164,16 +164,16 @@ without arguments fprime uses CuKa as default (Wave=1.54052A, E=8.0478keV)
         mainSizer.Add((10,15),0)
 
         selSizer = wx.BoxSizer(wx.HORIZONTAL)
-        selSizer.Add(wx.StaticText(parent=panel, label=' Wavelength:',
-            size=wx.Size(80,15)),0,wx.ALIGN_CENTER_VERTICAL)
+        selSizer.Add(wx.StaticText(parent=panel, label='   Wavelength:  '),0,
+            wx.ALIGN_CENTER_VERTICAL|wx.EXPAND)
         self.SpinText1 = wx.TextCtrl(id=wxID_SPINTEXT1, parent=panel, 
               size=wx.Size(100,20), value = "%6.4f" % (self.Wave),style=wx.TE_PROCESS_ENTER )
         selSizer.Add(self.SpinText1,0)
         self.SpinText1.SetToolTipString('Enter desired wavelength')
         self.SpinText1.Bind(wx.EVT_TEXT_ENTER, self.OnSpinText1, id=wxID_SPINTEXT1)
         
-        selSizer.Add(wx.StaticText(parent=panel, label=' Energy:',
-            size=wx.Size(80,15)),0,wx.ALIGN_CENTER_VERTICAL)
+        selSizer.Add(wx.StaticText(parent=panel, label='   Energy:  '),0,
+            wx.ALIGN_CENTER_VERTICAL|wx.EXPAND)
         self.SpinText2 = wx.TextCtrl(id=wxID_SPINTEXT2, parent=panel, 
               size=wx.Size(100,20), value = "%7.4f" % (self.Energy),style=wx.TE_PROCESS_ENTER) 
         selSizer.Add(self.SpinText2,0)
@@ -201,8 +201,8 @@ without arguments fprime uses CuKa as default (Wave=1.54052A, E=8.0478keV)
         mainSizer.Add((10,10),0)
         
         choiceSizer = wx.BoxSizer(wx.HORIZONTAL)
-        choiceSizer.Add(wx.StaticText(parent=panel, label=' Plot scales:',
-            size=wx.Size(80,15)),0,wx.ALIGN_CENTER_VERTICAL)
+        choiceSizer.Add(wx.StaticText(parent=panel, label='   Plot scales:  '),
+            0,wx.ALIGN_CENTER_VERTICAL|wx.EXPAND)
 
         self.choice1 = wx.ComboBox(id=wxID_FPRIMECHOICE1, parent=panel, value='Wavelength',
              choices=['Wavelength','Energy'],style=wx.CB_READONLY|wx.CB_DROPDOWN)
